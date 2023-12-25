8cabc81377c671fb0d6953b5f4ffa0586629b2e8
JianjunJin <jianjunjin@qq.com>
2023-12-25 15:40:23 -0500

    0.0.22: model proposal with contig depth ~ multiplicity: gaussian distribution -> multinomial distribution.

--- traversome/__init__.py
+++ traversome/__init__.py
@@ -5,7 +5,7 @@ Traversome: genome variant resolution and frequency estimation from genome assem
 """
 
 
-__version__ = "0.0.21.2"
+__version__ = "0.0.22"
 __author__ = "JianJun Jin and Deren Eaton"
 
 
